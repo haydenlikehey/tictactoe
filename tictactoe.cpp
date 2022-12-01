0db32e1b040c390da4c34e9fedc527bcfb822a53
Heyden <hgrace0001@gmail.com>
2022-11-30 18:19:52 -0600

    Work begins

---
+++ tictactoe.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+int main (int argc, char* argv[]) {
+
+  cout << "Tic-Tac-Toe"; << endl;
+  cout << "Here are the rules:\n" << endl;
+  cout << "\t-Decide amongst yourselves who will be player one\n and who will be player two" << endl;
+  cout << "\t-Player 1 will go first and input either a 2 dimensional coordinate into the console" << endl;
+  cout << "\t-This is done to select the coordinates of the space to place and \"X\"";
+  cout << "\t-Player 2 will follow for \"O\" and the cycle will continue until someone wins";
+  cout << "\t-The first to get three in a row wins\n\n\n\n";
+
+
+
+
+
+}
