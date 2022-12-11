57401827cc27cea639a12ec1ebb67e8ea399564d
Heyden <hgrace0001@gmail.com>
2022-12-10 22:28:11 -0600

    fx file gets game check function and main gets game loop

--- tictacfx.cpp
+++ tictacfx.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+
+int checkGameOverP1(int result1, int result2, int result3, int result4, int result5, int result6, int result7, int result8, int result9) {
+
+  int check1 = result1 + result2 + result3;
+  int check2 = result1 + result4 + result7;
+  int check3 = result1 + result5 + result9;
+  int check4 = result4 + result5 + result6;
+  int check5 = result7 + result8 + result9;
+  int check6 = result2 + result5 + result8;
+  int check7 = result3 + result6 + result9;
+  int check8 = result3 + result5 + result7;
+
+  if (check1 == 3 || check2 == 3 || check3 == 3 || check4 == 3 || check5 == 3 || check6 == 3 || check7 == 3 || check8 ==3) {
+    return 1;
+  }
+}
--- tictactoe.cpp
+++ tictactoe.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main (int argc, char* argv[]) {
 
-  cout << "Tic-Tac-Toe"; << endl;
+  cout << "Tic-Tac-Toe" << endl;
   cout << "Here are the rules:\n" << endl;
   cout << "\t-Decide amongst yourselves who will be player one\n and who will be player two" << endl;
   cout << "\t-Player 1 will go first and input either a 2 dimensional coordinate into the console" << endl;
@@ -28,6 +28,69 @@ int main (int argc, char* argv[]) {
     This just made me think of considering to use bitfields. If I can, I will
 
     Not much planned after that. Getting the whole github bullshit figured out tuckered me out*/
+
+start:
+  int board[3][3];
+  bool game = 1;
+
+/*
+  board[0][0] = 1;
+  board[0][1] = 1;
+  board[0][2] = 0;
+*/
+
+//Seeing what the result of bool math is
+//  int result = board[0][0] + board[0][1] + board[0][2];
+
+// cout << result;
+//result is 2 as expected
+
+
+  while (game == 1) {
+
+  loop1:
+    string choice1;
+    string choice2;
+
+    cout << "Player 1, input your choice:";
+    cin >> choice1 >> choice2;
+
+    switch (choice1 + "," + choice2) {
+      case "1,1":
+        board[0][0] = 1;
+        break;
+      case "1,2":
+        board[0][1] = 1;
+        break;
+      case "1,3":
+        board[0][2] = 1;
+        break;
+      case "2,1":
+        board[1][0] = 1;
+        break;
+      case "2,2":
+        board[1][1] = 1;
+        break;
+      case "2,3":
+        board[1][2] = 1;
+        break;
+      case "3,1":
+        board[2][0] = 1;
+        break;
+      case "3,2":
+        board[2][1] = 1;
+        break;
+      case "3,3":
+        board[2][2] = 1;
+        break;
+      default:
+        cout << "Please provide valid input";
+        goto loop1;
+
+    }
+
+
+  }
 
 
 }
