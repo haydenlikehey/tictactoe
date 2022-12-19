e488de5901be66bea0bab1a73b27632075ba74d6
Heyden <hgrace0001@gmail.com>
2022-12-18 22:04:36 -0600

    git test for linux

--- tictacfx.cpp
+++ tictacfx.cpp
@@ -21,6 +21,7 @@ int checkGameOverP1(int result1, int result2, int result3, int result4, int resu
   }
 }
 
+//small note to test git on linux
 
 int checkGameOverP2(int result1, int result2, int result3, int result4, int result5, int result6, int result7, int result8, int result9) {
 
