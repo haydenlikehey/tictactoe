acb37e1ff16fa0ef078a5bf57354e716fc3bfea2
Heyden <hgrace0001@gmail.com>
2022-12-18 22:17:03 -0600

    windows git integration test

--- tictacfx.cpp
+++ tictacfx.cpp
@@ -22,6 +22,7 @@ int checkGameOverP1(int result1, int result2, int result3, int result4, int resu
 }
 
 //small note to test git on linux
+//small note to see if changes sync on Windows (currently, sync doesn't end in the UI)
 
 int checkGameOverP2(int result1, int result2, int result3, int result4, int result5, int result6, int result7, int result8, int result9) {
 
