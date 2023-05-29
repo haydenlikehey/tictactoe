40e436ef6225ea8b6c12338f08380a4b7dc9efbc
Heyden <112825785+haydenlikehey@users.noreply.github.com>
2023-05-29 13:27:27 -0500

    Players can't overwrite moves now (no testing)

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -39,9 +39,10 @@ Other future iteration considerations:
 start:
   short board[9];
   short game = 0;
+  short turns = 0;
 
 
-  while (game == 0) {
+  while (turns < 10) {
 
   loop1:
     char choice1;
@@ -51,39 +52,39 @@ start:
 
     switch (choice1) {
       case '1':
-        board[0] = 1;
-        break;
+        if (board[0] == 0) {board[0] = 1; break;}
+        else {cout << "This space is taken." << endl; goto loop1; break;}
       case '2':
-        board[1] = 1;
-        break;
+        if (board[1] == 0) {board[1] = 1; break;}
+        else {cout << "This space is taken." << endl; goto loop1; break;}
       case '3':
-        board[2] = 1;
-        break;
+        if (board[2] == 0) {board[2] = 1; break;}
+        else {cout << "This space is taken." << endl; goto loop1; break;}
       case '4':
-        board[3] = 1;
-        break;
+        if (board[3] == 0) {board[3] = 1; break;}
+        else {cout << "This space is taken." << endl; goto loop1; break;}
       case '5':
-        board[4] = 1;
-        break;
+        if (board[4] == 0) {board[4] = 1; break;}
+        else {cout << "This space is taken." << endl; goto loop1; break;}
       case '6':
-        board[5] = 1;
-        break;
+        if (board[5] == 0) {board[5] = 1; break;}
+        else {cout << "This space is taken." << endl; goto loop1; break;}
       case '7':
-        board[6] = 1;
-        break;
+        if (board[6] == 0) {board[6] = 1; break;}
+        else {cout << "This space is taken." << endl; goto loop1; break;}
       case '8':
-        board[7] = 1;
-        break;
+        if (board[7] == 0) {board[7] = 1; break;}
+        else {cout << "This space is taken." << endl; goto loop1; break;}
       case '9':
-        board[8] = 1;
-        break;
+        if (board[8] == 0) {board[8] = 1; break;}
+        else {cout << "This space is taken." << endl; goto loop1; break;}
       default:
         cout << "Please provide valid input";
         goto loop1;
     }
 
     game = checkGameOverP1(board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8]);
-
+    turns++;
 
   loop2:
     char choice2;
@@ -91,47 +92,49 @@ start:
     cout << "Player 2, input your choice:";
     cin >> choice2;
 
-    switch (choice2) {
+     switch (choice2) {
       case '1':
-        board[0] = -1;
-        break;
+        if (board[0] == 0) {board[0] = -1; break;}
+        else {cout << "This space is taken." << endl; goto loop2; break;}
       case '2':
-        board[1] = -1;
-        break;
+        if (board[1] == 0) {board[1] = -1; break;}
+        else {cout << "This space is taken." << endl; goto loop2; break;}
       case '3':
-        board[2] = -1;
-        break;
+        if (board[2] == 0) {board[2] = -1; break;}
+        else {cout << "This space is taken." << endl; goto loop2; break;}
       case '4':
-        board[3] = -1;
-        break;
+        if (board[3] == 0) {board[3] = -1; break;}
+        else {cout << "This space is taken." << endl; goto loop2; break;}
       case '5':
-        board[4] = -1;
-        break;
+        if (board[4] == 0) {board[4] = -1; break;}
+        else {cout << "This space is taken." << endl; goto loop2; break;}
       case '6':
-        board[5] = -1;
-        break;
+        if (board[5] == 0) {board[5] = -1; break;}
+        else {cout << "This space is taken." << endl; goto loop2; break;}
       case '7':
-        board[6] = -1;
-        break;
+        if (board[6] == 0) {board[6] = -1; break;}
+        else {cout << "This space is taken." << endl; goto loop2; break;}
       case '8':
-        board[7] = -1;
-        break;
+        if (board[7] == 0) {board[7] = -1; break;}
+        else {cout << "This space is taken." << endl; goto loop2; break;}
       case '9':
-        board[8] = -1;
-        break;
+        if (board[8] == 0) {board[8] = -1; break;}
+        else {cout << "This space is taken." << endl; goto loop2; break;}
       default:
         cout << "Please provide valid input";
         goto loop2;
-    }
+    } 
 
     game = checkGameOverP2(board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8]);
-
+    turns++;
   }
 
   if (game == 1) {
     cout << "Okay, that's enough, player 1 wins!" << endl;
   } else if (game == -1) {
     cout << "Okay, that's enough, player 2 wins!" << endl;
+  } else if (game == 0) {
+    cout << "Looks like it's a tie!" << endl;
   }
 
 
