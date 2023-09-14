3662e7e9d2de3995878516231e28a819cd32b57b
Heyden <112825785+haydenlikehey@users.noreply.github.com>
2023-09-13 21:18:50 -0500

    Actual C++ conventions

--- tictacfx.cpp
+++ tictacfx.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-using namespace std;
+#include <vector>
+using std::cout, std::vector;
 
 
-short checkGameOverP1(short result1, short result2, short result3, short result4, short result5, short result6, short result7, short result8, short result9) {
+short checkGameOverP1(vector<int> result) {
 
-  if (result1 + result2 + result3 == 3 || result1 + result4 + result7 == 3 || result1 + result5 + result9 == 3 || result4 + result5 + result6 == 3 || result7 + result8 + result9 == 3 || result2 + result5 + result8 == 3 || result2 + result6 + result9 == 3 || result3 + result5 + result7 == 3) {
+  if (result[0] + result[1] + result[2] == 3 || result[0] + result[3] + result[6] == 3 || result[0] + result[4] + result[8] == 3 || result[3] + result[4] + result[5] == 3 || result[6] + result[7] + result[8] == 3 || result[1] + result[4] + result[7] == 3 || result[1] + result[5] + result[8] == 3 || result[2] + result[4] + result[6] == 3) {
     return 1;
   } else {
     return 0;
@@ -13,9 +14,9 @@ short checkGameOverP1(short result1, short result2, short result3, short result4
 
 
 
-short checkGameOverP2(short result1, short result2, short result3, short result4, short result5, short result6, short result7, short result8, short result9) {
+short checkGameOverP2(vector<int> result) {
 
-  if (result1 + result2 + result3 == -3 || result1 + result4 + result7 == -3 || result1 + result5 + result9 == -3 || result4 + result5 + result6 == -3 || result7 + result8 + result9 == -3 || result2 + result5 + result8 == -3 || result2 + result6 + result9 == -3 || result3 + result5 + result7 == -3) {
+  if (result[0] + result[1] + result[2] == -3 || result[0] + result[3] + result[6] == -3 || result[0] + result[4] + result[8] == -3 || result[3] + result[4] + result[5] == -3 || result[6] + result[7] + result[8] == -3 || result[1] + result[4] + result[7] == -3 || result[1] + result[5] + result[8] == -3 || result[2] + result[4] + result[6] == -3) {
     return -1;
   } else {
     return 0;
@@ -23,49 +24,49 @@ short checkGameOverP2(short result1, short result2, short result3, short result4
 }
 
 void printRules(void) {
-  cout << "Tic-Tac-Toe" << endl;
-  cout << "Here are the rules:\n" << endl;
-  cout << "\t-Decide amongst yourselves who will be player one\n\tand who will be player two" << endl;
-  cout << "\t-Player 1 will go first and input their desired move based on the numbering system below:\n" << endl;
-  cout << "     |     |     " << endl;
-  cout << "  1  |  2  |  3  " << endl;
-  cout << "_____|_____|_____" << endl;
-  cout << "     |     |     " << endl;
-  cout << "  4  |  5  |  6  " << endl;
-  cout << "_____|_____|_____" << endl;
-  cout << "     |     |     " << endl;
-  cout << "  7  |  8  |  9  " << endl;
-  cout << "     |     |     \n" << endl;
-  cout << "\t-This is done to select the space to place an \"X\"" << endl;
-  cout << "\t-Player 2 will follow for \"O\" and the cycle will continue until someone wins" << endl;
+  cout << "Tic-Tac-Toe" << "\n";
+  cout << "Here are the rules:\n" << "\n";
+  cout << "\t-Decide amongst yourselves who will be player one\n\tand who will be player two" << "\n";
+  cout << "\t-Player 1 will go first and input their desired move based on the numbering system below:\n" << "\n";
+  cout << "     |     |     " << "\n";
+  cout << "  1  |  2  |  3  " << "\n";
+  cout << "_____|_____|_____" << "\n";
+  cout << "     |     |     " << "\n";
+  cout << "  4  |  5  |  6  " << "\n";
+  cout << "_____|_____|_____" << "\n";
+  cout << "     |     |     " << "\n";
+  cout << "  7  |  8  |  9  " << "\n";
+  cout << "     |     |     \n" << "\n";
+  cout << "\t-This is done to select the space to place an \"X\"" << "\n";
+  cout << "\t-Player 2 will follow for \"O\" and the cycle will continue until someone wins" << "\n";
   cout << "\t-The first to get three in a row wins\n\n\n\n";
 }
 
-void printCurrentBoard(short result1, short result2, short result3, short result4, short result5, short result6, short result7, short result8, short result9) {
+void printCurrentBoard(vector<int> result) {
 
   char space1, space2, space3, space4, space5, space6, space7, space8, space9; 
 
-  if (result1 == 0) {space1 = '_';} else if (result1 == 1) {space1 = 'X';} else {space1 = 'O';}
-  if (result2 == 0) {space2 = '_';} else if (result2 == 1) {space2 = 'X';} else {space2 = 'O';}
-  if (result3 == 0) {space3 = '_';} else if (result3 == 1) {space3 = 'X';} else {space3 = 'O';}
-  if (result4 == 0) {space4 = '_';} else if (result4 == 1) {space4 = 'X';} else {space4 = 'O';}
-  if (result5 == 0) {space5 = '_';} else if (result5 == 1) {space5 = 'X';} else {space5 = 'O';}
-  if (result6 == 0) {space6 = '_';} else if (result6 == 1) {space6 = 'X';} else {space6 = 'O';}
-  if (result7 == 0) {space7 = '_';} else if (result7 == 1) {space7 = 'X';} else {space7 = 'O';}
-  if (result8 == 0) {space8 = '_';} else if (result8 == 1) {space8 = 'X';} else {space8 = 'O';}
-  if (result9 == 0) {space9 = '_';} else if (result9 == 1) {space9 = 'X';} else {space9 = 'O';}
+  if (result[0] == 0) {space1 = '_';} else if (result[0] == 1) {space1 = 'X';} else {space1 = 'O';}
+  if (result[1] == 0) {space2 = '_';} else if (result[1] == 1) {space2 = 'X';} else {space2 = 'O';}
+  if (result[2] == 0) {space3 = '_';} else if (result[2] == 1) {space3 = 'X';} else {space3 = 'O';}
+  if (result[3] == 0) {space4 = '_';} else if (result[3] == 1) {space4 = 'X';} else {space4 = 'O';}
+  if (result[4] == 0) {space5 = '_';} else if (result[4] == 1) {space5 = 'X';} else {space5 = 'O';}
+  if (result[5] == 0) {space6 = '_';} else if (result[5] == 1) {space6 = 'X';} else {space6 = 'O';}
+  if (result[6] == 0) {space7 = '_';} else if (result[6] == 1) {space7 = 'X';} else {space7 = 'O';}
+  if (result[7] == 0) {space8 = '_';} else if (result[7] == 1) {space8 = 'X';} else {space8 = 'O';}
+  if (result[8] == 0) {space9 = '_';} else if (result[8] == 1) {space9 = 'X';} else {space9 = 'O';}
 
   
 
-  cout << "     |     |     " << endl;
-  cout << "  " << space1 << "  |  " << space2 << "  |  " << space3 << "  " << endl;
-  cout << "_____|_____|_____" << endl;
-  cout << "     |     |     " << endl;
-  cout << "  " << space4 << "  |  " << space5 << "  |  " << space6 << "  " << endl;
-  cout << "_____|_____|_____" << endl;
-  cout << "     |     |     " << endl;
-  cout << "  " << space7 << "  |  " << space8 << "  |  " << space9 << "  " << endl;
-  cout << "     |     |     \n" << endl;
+  cout << "     |     |     " << "\n";
+  cout << "  " << space1 << "  |  " << space2 << "  |  " << space3 << "  " << "\n";
+  cout << "_____|_____|_____" << "\n";
+  cout << "     |     |     " << "\n";
+  cout << "  " << space4 << "  |  " << space5 << "  |  " << space6 << "  " << "\n";
+  cout << "_____|_____|_____" << "\n";
+  cout << "     |     |     " << "\n";
+  cout << "  " << space7 << "  |  " << space8 << "  |  " << space9 << "  " << "\n";
+  cout << "     |     |     \n" << "\n";
 
 
 }
--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <vector>
 #include "ttt.hpp"
-using namespace std;
+using std::cout, std::cin, std::vector;
 
 
 
@@ -8,7 +9,8 @@ int main (int argc, char* argv[]) {
 
 printRules();
 
-  short board[9] {};
+  vector<int> vboard(9, 0);
+  //short vboard[9] {};
   short game = 0;
   short turns = 0;
 
@@ -23,48 +25,48 @@ printRules();
 
     switch (choice1) {
       case '1':
-        if (board[0] != 0) {cout << "This space is taken." << endl; goto loop1;}
-        if (board[0] == 0) {board[0] = 1;}
+        if (vboard[0] != 0) {cout << "This space is taken." << "\n"; goto loop1;}
+        if (vboard[0] == 0) {vboard[0] = 1;}
         break;
       case '2':
-        if (board[1] != 0) {cout << "This space is taken." << endl; goto loop1;}
-        if (board[1] == 0) {board[1] = 1;}
+        if (vboard[1] != 0) {cout << "This space is taken." << "\n"; goto loop1;}
+        if (vboard[1] == 0) {vboard[1] = 1;}
         break;
       case '3':
-        if (board[2] != 0) {cout << "This space is taken." << endl; goto loop1;}
-        if (board[2] == 0) {board[2] = 1;}
+        if (vboard[2] != 0) {cout << "This space is taken." << "\n"; goto loop1;}
+        if (vboard[2] == 0) {vboard[2] = 1;}
         break;
       case '4':
-        if (board[3] != 0) {cout << "This space is taken." << endl; goto loop1;}
-        if (board[3] == 0) {board[3] = 1;}
+        if (vboard[3] != 0) {cout << "This space is taken." << "\n"; goto loop1;}
+        if (vboard[3] == 0) {vboard[3] = 1;}
         break;
       case '5':
-        if (board[4] != 0) {cout << "This space is taken." << endl; goto loop1;}
-        if (board[4] == 0) {board[4] = 1;}
+        if (vboard[4] != 0) {cout << "This space is taken." << "\n"; goto loop1;}
+        if (vboard[4] == 0) {vboard[4] = 1;}
         break;
       case '6':
-        if (board[5] != 0) {cout << "This space is taken." << endl; goto loop1;}
-        if (board[5] == 0) {board[5] = 1;}
+        if (vboard[5] != 0) {cout << "This space is taken." << "\n"; goto loop1;}
+        if (vboard[5] == 0) {vboard[5] = 1;}
         break;
       case '7':
-        if (board[6] != 0) {cout << "This space is taken." << endl; goto loop1;}
-        if (board[6] == 0) {board[6] = 1;}
+        if (vboard[6] != 0) {cout << "This space is taken." << "\n"; goto loop1;}
+        if (vboard[6] == 0) {vboard[6] = 1;}
         break;
       case '8':
-        if (board[7] != 0) {cout << "This space is taken." << endl; goto loop1;}
-        if (board[7] == 0) {board[7] = 1;}
+        if (vboard[7] != 0) {cout << "This space is taken." << "\n"; goto loop1;}
+        if (vboard[7] == 0) {vboard[7] = 1;}
         break;
       case '9':
-        if (board[8] != 0) {cout << "This space is taken." << endl; goto loop1;}
-        if (board[8] == 0) {board[8] = 1;}
+        if (vboard[8] != 0) {cout << "This space is taken." << "\n"; goto loop1;}
+        if (vboard[8] == 0) {vboard[8] = 1;}
         break;
       default:
         cout << "Please provide valid input";
         goto loop1;
     }
 
-    printCurrentBoard(board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8]);
-    game = checkGameOverP1(board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8]);
+    printCurrentBoard(vboard);
+    game = checkGameOverP1(vboard[0], vboard[1], vboard[2], vboard[3], vboard[4], vboard[5], vboard[6], vboard[7], vboard[8]);
     turns++;
     if (turns == 9) {continue;} //Last move victory failsafe
     if (game != 0) {break;} //Win condition breakout
@@ -77,57 +79,57 @@ printRules();
 
      switch (choice2) {
       case '1':
-        if (board[0] != 0) {cout << "This space is taken." << endl; goto loop2;}
-        if (board[0] == 0) {board[0] = -1;}
+        if (vboard[0] != 0) {cout << "This space is taken." << "\n"; goto loop2;}
+        if (vboard[0] == 0) {vboard[0] = -1;}
         break;
       case '2':
-        if (board[1] != 0) {cout << "This space is taken." << endl; goto loop2;}
-        if (board[1] == 0) {board[1] = -1;}
+        if (vboard[1] != 0) {cout << "This space is taken." << "\n"; goto loop2;}
+        if (vboard[1] == 0) {vboard[1] = -1;}
         break;
       case '3':
-        if (board[2] != 0) {cout << "This space is taken." << endl; goto loop2;}
-        if (board[2] == 0) {board[2] = -1;}
+        if (vboard[2] != 0) {cout << "This space is taken." << "\n"; goto loop2;}
+        if (vboard[2] == 0) {vboard[2] = -1;}
         break;
       case '4':
-        if (board[3] != 0) {cout << "This space is taken." << endl; goto loop2;}
-        if (board[3] == 0) {board[3] = -1;}
+        if (vboard[3] != 0) {cout << "This space is taken." << "\n"; goto loop2;}
+        if (vboard[3] == 0) {vboard[3] = -1;}
         break;
       case '5':
-        if (board[4] != 0) {cout << "This space is taken." << endl; goto loop2;}
-        if (board[4] == 0) {board[4] = -1;}
+        if (vboard[4] != 0) {cout << "This space is taken." << "\n"; goto loop2;}
+        if (vboard[4] == 0) {vboard[4] = -1;}
         break;
       case '6':
-        if (board[5] != 0) {cout << "This space is taken." << endl; goto loop2;}
-        if (board[5] == 0) {board[5] = -1;}
+        if (vboard[5] != 0) {cout << "This space is taken." << "\n"; goto loop2;}
+        if (vboard[5] == 0) {vboard[5] = -1;}
         break;
       case '7':
-        if (board[6] != 0) {cout << "This space is taken." << endl; goto loop2;}
-        if (board[6] == 0) {board[6] = -1;}
+        if (vboard[6] != 0) {cout << "This space is taken." << "\n"; goto loop2;}
+        if (vboard[6] == 0) {vboard[6] = -1;}
         break;
       case '8':
-        if (board[7] != 0) {cout << "This space is taken." << endl; goto loop2;}
-        if (board[7] == 0) {board[7] = -1;}
+        if (vboard[7] != 0) {cout << "This space is taken." << "\n"; goto loop2;}
+        if (vboard[7] == 0) {vboard[7] = -1;}
         break;
       case '9':
-        if (board[8] != 0) {cout << "This space is taken." << endl; goto loop2;}
-        if (board[8] == 0) {board[8] = -1;}
+        if (vboard[8] != 0) {cout << "This space is taken." << "\n"; goto loop2;}
+        if (vboard[8] == 0) {vboard[8] = -1;}
         break;
       default:
         cout << "Please provide valid input";
         goto loop2;
     } 
-    printCurrentBoard(board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8]);
-    game = checkGameOverP2(board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8]);
+    printCurrentBoard(vboard);
+    game = checkGameOverP2(vboard);
     turns++;
     if (game != 0) {break;}
   }
 
   if (game == 1) {
-    cout << "Okay, that's enough, player 1 wins!" << endl;
+    cout << "Okay, that's enough, player 1 wins!" << "\n";
   } else if (game == -1) {
-    cout << "Okay, that's enough, player 2 wins!" << endl;
+    cout << "Okay, that's enough, player 2 wins!" << "\n";
   } else if (game == 0) {
-    cout << "Looks like it's a tie!" << endl;
+    cout << "Looks like it's a tie!" << "\n";
   }
 
 
