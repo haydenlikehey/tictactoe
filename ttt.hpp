873e8f6b49b958404343b2828f452374d4bb6764
Heyden <112825785+haydenlikehey@users.noreply.github.com>
2024-03-10 22:41:11 -0500

    Abandoning C++ conventions

--- ttt.hpp
+++ ttt.hpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
-using std::cout, std::cin, std::vector, std::setw;
+using std::cout, std::cin, std::setw;
 
 short checkGameOverP1(int* result);
 short checkGameOverP2(int* result);
